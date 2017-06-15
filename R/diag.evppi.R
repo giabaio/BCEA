11040f735890765b047e8b3a02134fb6e52ddb6c
Gianluca Baio <g.baio@ucl.ac.uk>
2017-06-15 11:05:56 +0100

    updates diag.evppi

--- R/diag.evppi.R
+++ R/diag.evppi.R
@@ -7,7 +7,7 @@ diag.evppi <- function(x,y,diag=c("residuals","qqplot"),int=1){
   if (int>1 & dim(x$fitted.costs)[2]==1) {stop("There is only one comparison possible, so 'int' should be set to 1 (default)")}
   res <- ifelse(isTRUE(pmatch(diag,c("residuals","qqplot"))==2),FALSE,TRUE)
   if(res){
-    par(mfrow=c(1,2))
+    op <- par(mfrow=c(1,2))
     n <- dim(x$fitted.costs)[1]
     fitted <- x$fitted.costs[,int]
     residual <- as.matrix(y$delta.c)[x$select,int]-fitted
@@ -17,10 +17,12 @@ diag.evppi <- function(x,y,diag=c("residuals","qqplot"),int=1){
     residual <- as.matrix(y$delta.e)[x$select,int]-fitted
     plot(fitted,residual,xlab="Fitted values",
          ylab="Residuals",main="Residual plot for effects",cex=.8);abline(h=0)
+    par(op)
   }else{
-    par(mfrow=c(1,2))
+    op <- par(mfrow=c(1,2))
     qqnorm(x$fitted.costs[,int],main="Normal Q-Q plot \n(costs)"); qqline(x$fitted.costs[,int])
     qqnorm(x$fitted.effects[,int],main="Normal Q-Q plot \n(effects)"); qqline(x$fitted.effects[,int])
+    par(op)
   }
   
 }
