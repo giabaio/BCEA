685ddfcba5f4b4a6fbdf4a4310086129984b0afc
Gianluca Baio <g.baio@ucl.ac.uk>
2017-06-16 10:53:40 +0100

    update CreateInputs

--- R/CreateInputs.R
+++ R/CreateInputs.R
@@ -1,39 +1,88 @@
 ######CreateInputs##############################################################################################
+#CreateInputs <- function(x) {
+#  # Utility function --- creates inputs for the EVPPI
+#  # First checks whether the model is run with JAGS or BUGS
+#  # 1. checks whether each model has been run using JAGS or BUGS or Stan  
+#  if(class(x)=="rjags") {
+#    if(!isTRUE(requireNamespace("R2jags",quietly=TRUE))) {
+#      stop("You need to install the package 'R2jags'. Please run in your R terminal:\n install.packages('R2jags')")
+#    }
+#    if (isTRUE(requireNamespace("R2jags",quietly=TRUE))) {
+#      mdl <- x$BUGSoutput      
+#    }
+#  }
+#  if(class(x)=="bugs") {  	# if model is run using R2WinBUGS/R2OpenBUGS
+#    if(!isTRUE(requireNamespace("R2OpenBUGS",quietly=TRUE))) {
+#      stop("You need to install the package 'R2OpenBUGS'. Please run in your R terminal:\n install.packages('R2OpenBUGS')")
+#    }
+#    if(isTRUE(requireNamespace("R2OpenBUGS",quietly=TRUE))) {
+#      mdl <- x
+#    }
+#  }
+#  if (class(x)%in%c("bugs","rjags")) {
+#    # Defines the inputs matrix  
+#    inputs <- mdl$sims.matrix
+#    # If the deviance is computed, then removes it 
+#    if("deviance"%in%colnames(inputs)) {
+#      w <- which(colnames(inputs)=="deviance")
+#      inputs <- inputs[,-w]
+#    }
+#  }
+#  if(class(x)=="stanfit") { # if the model is run with Stan
+#    inputs <- as.data.frame(x)
+#    # Removes the likelihood, which is never a relevant parameters for HE evaluation
+#    inputs <- inputs[,-which(colnames(inputs)=="lp__")]
+#  }
+#  # 2. saves the output to a relevant list
+#  pars <- colnames(data.frame(inputs))
+#  list(mat=data.frame(inputs),parameters=pars)
+#}
+
+
 CreateInputs <- function(x) {
-  # Utility function --- creates inputs for the EVPPI
-  # First checks whether the model is run with JAGS or BUGS
-  # 1. checks whether each model has been run using JAGS or BUGS or Stan  
-  if(class(x)=="rjags") {
-    if(!isTRUE(requireNamespace("R2jags",quietly=TRUE))) {
-      stop("You need to install the package 'R2jags'. Please run in your R terminal:\n install.packages('R2jags')")
-    }
-    if (isTRUE(requireNamespace("R2jags",quietly=TRUE))) {
-      mdl <- x$BUGSoutput      
-    }
-  }
-  if(class(x)=="bugs") {  	# if model is run using R2WinBUGS/R2OpenBUGS
-    if(!isTRUE(requireNamespace("R2OpenBUGS",quietly=TRUE))) {
-      stop("You need to install the package 'R2OpenBUGS'. Please run in your R terminal:\n install.packages('R2OpenBUGS')")
-    }
-    if(isTRUE(requireNamespace("R2OpenBUGS",quietly=TRUE))) {
-      mdl <- x
-    }
-  }
-  if (class(x)%in%c("bugs","rjags")) {
-    # Defines the inputs matrix  
-    inputs <- mdl$sims.matrix
-    # If the deviance is computed, then removes it 
-    if("deviance"%in%colnames(inputs)) {
-      w <- which(colnames(inputs)=="deviance")
-      inputs <- inputs[,-w]
-    }
-  }
-  if(class(x)=="stanfit") { # if the model is run with Stan
-    inputs <- as.data.frame(x)
-    # Removes the likelihood, which is never a relevant parameters for HE evaluation
-    inputs <- inputs[,-which(colnames(inputs)=="lp__")]
-  }
-  # 2. saves the output to a relevant list
-  pars <- colnames(data.frame(inputs))
-  list(mat=data.frame(inputs),parameters=pars)
+   # Utility function --- creates inputs for the EVPPI
+   if(class(x)=="rjags") {
+      inputs <- x$BUGSoutput$sims.matrix
+   }
+   if(class(x)=="bugs") {
+      inputs <- x$sims.matrix
+   }
+   if(class(x)=="stanfit") { 
+      inputs <- x
+   }
+   # Removes the deviance (which is not relevant for VOI computations
+   if (class(x)%in%c("bugs","rjags")) {
+      if("deviance"%in%colnames(inputs)) {
+        inputs <- inputs[,-which(colnames(inputs)=="deviance")]
+      }
+      else {
+         if(class(x)=="stanfit") {
+            inputs <- inputs[,-which(colnames(inputs)=="lp__")]
+         }
+      }
+   }
+   
+   # Now removes redundant parameters (linear combination of columns or columns that are constant)
+   # Code by Mark Strong
+   sets=colnames(inputs)
+   constantParams <- (apply(inputs, 2, var) == 0)
+   if (sum(constantParams) > 0) sets <- sets[-which(constantParams)] # remove constants
+   paramSet <- cbind(cbind(inputs)[, sets, drop=FALSE]) # now with constants removed
+   rankifremoved <- sapply(1:NCOL(paramSet), function (x) qr(paramSet[,-x])$rank)
+   while(length(unique(rankifremoved)) > 1) {
+      linearCombs <- which(rankifremoved == max(rankifremoved))
+      print(linearCombs)
+      print(paste("Linear dependence: removing column", colnames(paramSet)[max(linearCombs)]))
+      paramSet <- cbind(paramSet[, -max(linearCombs), drop=FALSE])
+      rankifremoved <- sapply(1:NCOL(paramSet), function (x) qr(paramSet[,-x])$rank)
+   }
+   while(qr(paramSet)$rank == rankifremoved[1]) {
+      print(paste("Linear dependence... removing column", colnames(paramSet)[1]))
+      paramSet <- cbind(paramSet[, -1, drop=FALSE]) # special case only lincomb left
+      rankifremoved <- sapply(1:NCOL(paramSet), function (x) qr(paramSet[,-x])$rank)
+   }
+
+   # Now saves the output to a relevant list
+   list(mat=data.frame(paramSet),parameters=colnames(data.frame(paramSet)))
 }
+
