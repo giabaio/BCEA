617a4730d90cb0152b0a669b56c4939b9d31ed33
Gianluca Baio <gianluca@statistica.it>
2018-12-11 15:56:11 +0000

    Test make.report

--- R/make.report.R
+++ R/make.report.R
@@ -57,5 +57,5 @@ make.report=function(he,evppi=NULL,ext="pdf",...) {
                                         docx=rmarkdown::word_document())
   ))
   file.copy(out, file, overwrite=TRUE)
-  cat(paste0("The report is saved in the file ",file,"/n"))
+  cat(paste0("The report is saved in the file ",file,"\n"))
 }
