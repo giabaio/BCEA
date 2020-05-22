cc180ebdb924d161af7c1a8a4b0860ba7d818dfa
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-05-22 09:06:51 +0100

    # now passes all error tests
    several small refactoring (safer)
    
    * change from . to _ separators
    * c,e to cost, eff mainly because c is a built in function
    * error asserts at top of bcea.default

--- R/bcea.default.R
+++ R/bcea.default.R
@@ -5,8 +5,8 @@
 #'
 #' INPUTS:
 #' 1. Two objects (e,c). These can be directly computed in a simulation object "sim" from JAGS/BUGS, 
-#'    or derived by postprocessing of "sim" in R. The objects (e,c) have dimension (n.sim x number of 
-#'    interventions) and contain n.sim simulated values for the measures of effectiveness and costs 
+#'    or derived by postprocessing of "sim" in R. The objects (e,c) have dimension (n_sim x number of 
+#'    interventions) and contain n_sim simulated values for the measures of effectiveness and costs 
 #'    for each intervention being compared. 
 #' 2. The reference intervention as a numeric value. Each intervention is a column in the matrices e 
 #'    and c so if ref=1 the first column is assumed to be associated with the reference intervention. 
@@ -17,13 +17,13 @@
 #'    is provided, then it is assumed Kmax=50000.
 #' 5. A(n optional) vector wtp including the values of the willingness to pay grid. If not specified
 #'    then BCEA will construct a grid of 501 values from 0 to Kmax. This option is useful when 
-#'    performing intensive computations (eg for the EVPPI)
+#'    performing intensive computations (e.g. for the EVPPI)
 #'
 #' @return Graphs & computed values for CE Plane, ICER, EIB, CEAC, EVPI 
 #' @export
 #'
-bcea.default <- function(e,
-                         c,
+bcea.default <- function(eff,
+                         cost,
                          ref = 1,
                          interventions = NULL,
                          Kmax = 50000,
@@ -36,50 +36,49 @@ bcea.default <- function(e,
   ##TODO: can we dispatch directly on jags/BUGS output?
   ##TODO: there several n.comparator == 1, >1 bits. can we improve this?
   
-  # Set the working directory to wherever the user is working, if not externally set
-  ##TODO: where is this used? on.exit()?
-  # if(!exists("working.dir")){working.dir <- here::here()}
+  if (!is.matrix(cost) | !is.matrix(eff)) stop("eff and cost must be matrices.")
+  if (ncol(cost) == 1 | ncol(eff) == 1) stop("Require at least 2 comparators.")
+  if (!is.null(interventions) & length(interventions) != ncol(eff)) stop("interventions names wrong length.")
+  if (any(dim(eff) != dim(cost))) stop("eff and cost are not the same dimensions.")
   
+  if (!is.double(ref) | ref < 1 | ref > ncol(eff)) stop("reference is not in available interventions.")
+
   # Number of simulations & interventions analysed
-  n.sim <- dim(e)[1]
-  n.comparators <- dim(e)[2]
+  n_sim <- dim(eff)[1]
+  n_comparators <- dim(eff)[2]
   
-  # Define reference & comparator intervention (different labels can be given here if available!)
-  if (!is.null(interventions) & length(interventions) != ncol(e)) stop ("interventions names wrong length.")
+  # Define reference & comparator intervention
+  # (different labels can be given here if available!)
   
-  if (any(dim(e) != dim(c))) stop("e and c are not the same dimensions.")
-  
-  ints <- 1:n.comparators
+  ints <- 1:n_comparators
   
   if (is.null(interventions))
     { interventions <- paste("intervention", ints) }
   
-  
   # Define intervention i as the reference 
-  # where i can be a number in [1,...,n.comparators])
-  # and the other(s) as comparator(s). Default is the first intervention (first column of e or c)
-  
-  ##TODO: check that ref is in ints?
+  # where i can be a number in [1,...,n_comparators])
+  # and the other(s) as comparator(s)
+  # Default is the first intervention (first column of eff or cost)
   
   comp <- ints[-ref]
-  n.comparisons <- n.comparators - 1
+  n_comparisons <- n_comparators - 1
   
   # Compute Effectiveness & Cost differentials (wrt to reference intervention)
-  delta.e <- e[, ref] - e[, comp]
-  delta.c <- c[, ref] - c[, comp]
+  delta_e <- eff[, ref] - eff[, comp]
+  delta_c <- cost[, ref] - cost[, comp]
   
   ##TODO: replace with
-  # ICER <- compute_ICER(delta.e, delta.c)
+  # ICER <- compute_ICER(delta_e, delta_c)
   
-  if(n.comparisons == 1) {
-    ICER <- mean(delta.c)/mean(delta.e)
+  if(n_comparisons == 1) {
+    ICER <- mean(delta_c)/mean(delta_e)
   }
-  if(n.comparisons > 1) {
-    ICER <- colMeans(delta.c)/colMeans(delta.e) #apply(delta.c,2,mean)/apply(delta.e,2,mean)
+  if(n_comparisons > 1) {
+    ICER <- colMeans(delta_c)/colMeans(delta_e) #apply(delta_c,2,mean)/apply(delta_e,2,mean)
   }
 
   # Compute and plot CEAC & EIB
-  if(!exists("Kmax")){Kmax <- 50000}
+  if(!exists("Kmax")){ Kmax <- 50000}
   
   # Lets you select the willingness to pay grid
   # useful when doing EVPPI (computationally intensive)
@@ -103,41 +102,41 @@ bcea.default <- function(e,
   # get_kstar()
   # get_best_EIB()
   
-  if(n.comparisons == 1) {
-    ib <- scale(k %*% t(delta.e), delta.c, scale = FALSE)
+  if(n_comparisons == 1) {
+    ib <- scale(k %*% t(delta_e), delta_c, scale = FALSE)
     ceac <- rowMeans(ib > 0) #apply(ib>0,1,mean)
   }
-  if(n.comparisons > 1) { 
-    ib <- array(rep(delta.e, K)*rep(k, each=n.sim*n.comparisons)-as.vector(delta.c),
-                dim=c(n.sim, n.comparisons, K))
+  if(n_comparisons > 1) { 
+    ib <- array(rep(delta_e, K)*rep(k, each = n_sim*n_comparisons) - as.vector(delta_c),
+                dim = c(n_sim, n_comparisons, K))
     ib <- aperm(ib, c(3,1,2))
-    ###          ib <- sweep(apply(delta.e,c(1,2),function(x) k%*%t(x)),c(2,3),delta.c,"-")
+    ###          ib <- sweep(apply(delta_e,c(1,2),function(x) k%*%t(x)),c(2,3),delta_c,"-")
     ceac <- apply(ib > 0, c(1,3), mean)
   }
   
   # Select the best option for each value of the willingness to pay parameter
-  if(n.comparisons == 1) {
+  if(n_comparisons == 1) {
     eib <- rowMeans(ib)  #apply(ib,1,mean)
-    best <- rep(ref,K)
+    best <- rep(ref, K)
     best[which(eib < 0)] <- comp
     ## Finds the k for which the optimal decision changes
     check <- c(0, diff(best))
     kstar <- k[check != 0]
   }
-  if(n.comparisons > 1) {
-    eib <- apply(ib, 3, function(x) apply(x,1,mean))
+  if(n_comparisons > 1) {
+    eib <- apply(ib, 3, function(x) apply(x, 1, mean))
     
     if (is.null(dim(eib))) {
-      tmp <- min(eib)
+      tmp  <- min(eib)
       tmp2 <- which.min(eib)	
     } else {
-      tmp <- apply(eib,1,min)
-      tmp2 <- apply(eib,1,which.min)
+      tmp <- apply(eib, 1, min)
+      tmp2 <- apply(eib, 1, which.min)
     }
     
     best <- ifelse(tmp > 0,ref,comp[tmp2])
     # Finds the k for which the optimal decision changes
-    check <- c(0,diff(best))
+    check <- c(0, diff(best))
     kstar <- k[check != 0]
   }
   
@@ -145,29 +144,29 @@ bcea.default <- function(e,
   ##TODO: replace with:
   # EVPI <- compute_EVPI()
   
-  U <- array(rep(e, K)*rep(k, each=n.sim*n.comparators) - as.vector(c),
-             dim=c(n.sim, n.comparators, K))
+  U <- array(rep(eff, K)*rep(k, each = n_sim*n_comparators) - as.vector(cost),
+             dim=c(n_sim, n_comparators, K))
   U <- aperm(U, c(1,3,2))
   
   rowMax <- function(x){do.call(pmax, as.data.frame(x))}
   
-  Ustar <- vi <- ol <- matrix(NA,n.sim,K) 
+  Ustar <- vi <- ol <- matrix(NA, n_sim, K) 
   
-  for (i in 1:K) {
-    Ustar[,i] <- rowMax(U[,i,])
-    cmd <- paste("ol[,i] <- Ustar[,i] - U[,i,",best[i],"]",sep="")
-    eval(parse(text=cmd))     
-    vi[,i] <- Ustar[,i] - max(apply(U[,i,],2,mean))
+  for (i in seq_len(K)) {
+    Ustar[, i] <- rowMax(U[, i,])
+    cmd <- paste("ol[, i] <- Ustar[, i] - U[, i,", best[i], "]", sep = "")
+    eval(parse(text = cmd))     
+    vi[, i] <- Ustar[, i] - max(apply(U[, i,], 2, mean))
   }
   evi <- colMeans(ol)
   
   ## Outputs of the function
   he <- list(
-    n.sim = n.sim,
-    n.comparators = n.comparators,
-    n.comparisons = n.comparisons,
-    delta.e = delta.e,
-    delta.c = delta.c,
+    n_sim = n_sim,
+    n_comparators = n_comparators,
+    n_comparisons = n_comparisons,
+    delta_e = delta_e,
+    delta_c = delta_c,
     ICER = ICER,
     Kmax = Kmax,
     k = k,
@@ -185,8 +184,8 @@ bcea.default <- function(e,
     ref = ref,
     comp = comp,
     step = step,
-    e = e,
-    c = c)
+    e = eff,
+    c = cost)
   
   class(he) <- "bcea"
 
--- tests/testthat/test-bcea.R
+++ tests/testthat/test-bcea.R
@@ -1,30 +1,69 @@
 
-library(BCEA)
+# library(BCEA)
 
-data(Vaccine)
+load("ce.RData")
 
-res <- bcea(e = e,
-            c = c,    
-            ref = 2,  
-            interventions = treats,
-            Kmax = 50000,
-            plot = FALSE)
 
-test_that("inputs", {
+test_that("input errors", {
   
-  expect_error(bcea(e, c[-1, ], plot = FALSE),
-               regexp = "e and c are not the same dimensions.")
+  expect_error(
+    bcea(eff, cost[c(1,2,1), ],
+         plot = FALSE),
+    regexp = "eff and cost are not the same dimensions.")
   
-  expect_error(bcea(e, c[, -1, drop=FALSE], plot = FALSE),
-               regexp = "e and c are not the same dimensions.")
+  expect_error(
+    bcea(
+      eff, cost[, c(1,2,1)],
+      plot = FALSE),
+    regexp = "eff and cost are not the same dimensions.")
   
-  expect_error(bcea(e[-1, ], c, plot = FALSE),
-               regexp = "e and c are not the same dimensions.")
+  expect_error(
+    bcea(eff[c(1,2,1), ], cost,
+         plot = FALSE),
+    regexp = "eff and cost are not the same dimensions.")
   
-  expect_error(bcea(e[, -1, drop=FALSE], c, plot = FALSE),
-               regexp = "e and c are not the same dimensions.") 
+  expect_error(
+    bcea(eff[, c(1,2,1)], cost,
+         plot = FALSE),
+    regexp = "eff and cost are not the same dimensions.") 
   
-  expect_error(bcea(e, c, interventions = c("aaa"), plot = FALSE),
-               regexp = "interventions names wrong length.")
+  expect_error(
+    bcea(eff, cost,
+         interventions = c("aaa"),
+         plot = FALSE),
+    regexp = "interventions names wrong length.")
   
+  expect_error(
+    bcea(eff, cost,
+         ref = 0,
+         plot = FALSE),
+    regexp = "reference is not in available interventions.")
+  
+  expect_error(
+    bcea(eff, cost,
+         ref = 3,
+         plot = FALSE),
+    regexp = "reference is not in available interventions.")
+  
+  # expect_error(bcea(e, c, ref = 1.1, plot = FALSE),
+  #              regexp = "reference is not in available interventions.")
+  
+  expect_error(
+    bcea(c(0,0), c(1,2),
+         plot = FALSE),
+    regexp = "eff and cost must be matrices.")
+  
+  expect_error(
+    bcea(matrix(c(0,0)), matrix(c(1,2)),
+         plot = FALSE),
+    regexp = "Require at least 2 comparators.")
+})
+
+
+test_that("basic input values", {
+  
+  # dat <- data.frame(eff = c(0,0),
+  #                   c = c(1,2))
+  # 
+  # bcea(dat$eff, dat$c)
 })
