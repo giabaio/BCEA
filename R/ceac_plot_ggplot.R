2ab2eea891382930c6e92bc171caa934fa75a8c1
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-07-02 16:15:56 +0100

    write own theme ceac function

--- R/ceac_plot_ggplot.R
+++ R/ceac_plot_ggplot.R
@@ -29,34 +29,17 @@
                                       each = length(he$k))))
   
   graph_params <- helper_ggplot_params(he, graph_params)
-  
   legend_params <- make_legend(pos_legend)
-  
-  theme_params <- purrr::keep(extra_params, is.theme)
+  theme_add <- purrr::keep(extra_params, is.theme)
   
   ggplot(data_psa, aes(k, ceac)) +
     geom_line(aes(linetype = comparison,
                   colour = factor(comparison))) +
-    theme_bw() + 
-    theme_params +                                         # theme
+    theme_ceac() + 
+    theme_add +                                            # theme
     scale_y_continuous(limits = c(0, 1)) +
     do.call(labs, graph_params$annot) +                    # text
-    do.call(theme,                                         # legend
-            modifyList(
-              list(legend.title = element_blank(),
-                   legend.background = element_blank(),
-                   text = element_text(size = 11),
-                   legend.key.size = grid::unit(.66, "lines"),
-                   legend.spacing = grid::unit(-1.25, "line"),
-                   panel.grid = element_blank(),
-                   legend.key = element_blank(),
-                   legend.text.align = 0,
-                   plot.title = element_text(
-                     lineheight = 1.05,
-                     face = "bold",
-                     size = 14.3,
-                     hjust = 0.5)),
-              legend_params)) +
+    do.call(theme, legend_params) +                        # legend
     scale_linetype_manual("",                              # lines
                           labels = graph_params$plot$labels,
                           values = graph_params$plot$line$types) +
