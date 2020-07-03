8025bf1df6b8daef659f3c20de5f6a08b2cec585
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-07-03 12:51:33 +0100

    include a ggplot test using vdiffr

---
+++ tests/testthat/test-ceac_plot_ggplot.R
@@ -0,0 +1,16 @@
+context("ceac_plot")
+
+# vdiffr::manage_cases(filter = "ceac")
+
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+
+load("ce.RData")
+he <- BCEA::bcea(eff, cost)
+
+test_that("ceac.plot_ggplot draws correctly", {
+  
+  ceac_plot <- ceac.plot(he, graph = "ggplot2", title = "my title")
+  vdiffr::expect_doppelganger(title = "ceac plot ggplot", fig = ceac_plot)
+})
