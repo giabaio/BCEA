bbf7c72dd3228f5ddc7be78c8313186a4290e344
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-07-21 14:22:36 +0100

    test newline

--- tests/testthat/test-ceac_plot_ggplot.R
+++ tests/testthat/test-ceac_plot_ggplot.R
@@ -15,4 +15,4 @@ test_that("ceac.plot_ggplot draws correctly", {
   
   ceac_plot <- ceac.plot(he, graph = "ggplot2", title = "my title")
   vdiffr::expect_doppelganger(title = "ceac plot ggplot", fig = ceac_plot)
-})j
+})
