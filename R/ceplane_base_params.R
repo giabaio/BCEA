5dcbc6ca9f57981a77622f3572eb07187274ecca
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-08-02 19:29:15 +0100

    simplify ceplane_base_params(). need to check this still works

--- R/ceplane_base_params.R
+++ R/ceplane_base_params.R
@@ -15,26 +15,6 @@ ceplane_base_params <- function(he,
                         comparison,
                         graph_params)
   
-  # same order and length
-  
-  param_names <- union(names(axes_params),
-                       names(plot_params))
-  
-  empty_list <-
-    vector("list", length = length(param_names))
-  
-  names(empty_list) <- param_names
-  
-  axes_params <- 
-    modifyList(empty_list, axes_params)
-  
-  plot_params <- 
-    modifyList(empty_list, plot_params)
-  
-  # merge lists
-  mapply(FUN = c,
-         plot_params,
-         axes_params,
-         SIMPLIFY = FALSE)
+  modifyList(plot_params, axes_params)
 }
 
