e6ec8bee6977e2d6d92ddf8777c78dbe91aa3c77
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-08-03 11:01:44 +0100

    function duplication deleted

--- R/subset_by_comparisons.R
+++ R/subset_by_comparisons.R
@@ -9,10 +9,21 @@ subset_by_comparisons <- function(he, comparison) {
   he$delta_c <- as.matrix(he$delta_c)[, comparison, drop = FALSE]
   
   he$n_comparisons <- length(comparison)
+  he$n_comparators <- length(comparison) + 1
   
   he$icer <- he$icer[comparison]
   
-  he$comp <- comparison
+  he$comp <- he$comp[comparison]
+  
+  he$interventions <- he$interventions[sort(c(he$ref, he$comp))]
+  he$ICER <- he$ICER[comparison]
+  he$ib <- he$ib[, , comparison]
+  he$eib <- he$eib[, comparison]
+  he$U <- he$U[, , sort(c(he$ref, comparison + 1))]
+  he$ceac <- he$ceac[, comparison]
+  he$mod <- TRUE
   
   return(he)
 }
+
+
