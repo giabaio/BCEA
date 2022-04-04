722312e80b6d077cd16614de2b13e685d0e0dfbb
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-04-04 07:59:15 +0100

    deleted all geom_contour bits. relying solely on stat_density2d() hopefully!

--- R/contour_ggplot.R
+++ R/contour_ggplot.R
@@ -5,12 +5,11 @@
 #' @template args-he
 #' @param params Plot parameters
 #' @param extra_args additional arguments
-#' @param comparison Comparison interventions; default 1
 #' 
 #' @importFrom grid unit
 #' @import ggplot2
 #' 
-contour_ggplot <- function(he, params, extra_args, comparison = 1) {
+contour_ggplot <- function(he, params, extra_args) {
   
   xlab <- params$xlab
   ylab <- params$ylab
@@ -32,124 +31,49 @@ contour_ggplot <- function(he, params, extra_args, comparison = 1) {
     if (nlevels == 0)
       nlevels <- 1
   }
+  
   points.colour <- 
     if (nlevels == 1) {
       "black"
     } else {
       "grey"}
   
-  if (he$n_comparisons == 1) {
-    kd <- data.frame(e = unlist(he$delta_e),
-                     c = unlist(he$delta_c))
-    
-    labels.df <-
-      data.frame(
-        he = c(ranges$e[2], ranges$e[1], ranges$e[1], ranges$e[2]),
-        y = c(rep(ranges$c[2], 2), rep(ranges$c[1], 2)),
-        label = c(p.ne, p.nw, p.sw, p.se),
-        hjust = as.factor(c(1, 0, 0, 1)))
-
-    ceplane <-
-      ggplot(kd, aes(.data$e, .data$c)) +
-      geom_point(size = 1, color = points.colour) +
-      scale_x_continuous(limits = ranges$e) +
-      scale_y_continuous(limits = ranges$c) +
-      geom_text(
-        data = labels.df,
-        aes(
-          x = .data$he,
-          y = .data$y,
-          hjust = .data$hjust,
-          label = .data$label),
-        parse = TRUE,
-        size = rel(3.5))
-    
-    if (!is.null(scale) && requireNamespace("MASS", quietly = TRUE)) {
-      density <-
-        MASS::kde2d(as.matrix(he$delta_e),
-                    as.matrix(he$delta_c),
-                    n = 300,
-                    h = c(sd(as.matrix(he$delta_e)) / scale,
-                          sd(as.matrix(he$delta_c)) / scale))
-      
-      density <-
-        data.frame(expand.grid(e = density$x,
-                               c = density$y),
-                   z = as.vector(density$z))
-      ceplane <-
-        ceplane +
-        geom_contour(
-          aes(z = .data$z),
-          data = density,
-          colour = "black",
-          bins = nlevels)
-    } else {
-      ceplane <- ceplane + stat_density2d(color = "black")
-    }
-
-  }
-  
-  
-  if (he$n_comparisons > 1 & is.null(comparison)) {
-    kd <- data.frame(
-      delta_e = c(as.matrix(he$delta_e)),
-      delta_c = c(as.matrix(he$delta_c)),
-      comparison =
-        as.factor(sort(
-          rep(1:he$n_comparisons, dim(as.matrix(he$delta_e))[1])))
-    )
-    
-    ceplane <-
-      ggplot(kd,
-             aes(x = .data$delta_e, y = .data$delta_c,
-                 col = .data$comparison)) +
-      geom_point(size = 1) +
-      scale_color_manual(label = comparisons.label,
-                         values = colors.label,
-                         na.value = "black") +
-      scale_x_continuous(limits = ranges$e) +
-      scale_y_continuous(limits = ranges$c)
-    
-    if (!is.null(scale) && requireNamespace("MASS", quietly = TRUE)) {
-      densitydf <- data.frame()
-      
-      for (i in seq_len(he$n_comparison)) {
-        temp <-
-          MASS::kde2d(
-            as.matrix(he$delta_e)[, i],
-            as.matrix(he$delta_c)[, i],
-            n = 300,
-            h = c(sd(as.matrix(he$delta_e)[, i]) / scale,
-                  sd(as.matrix(he$delta_c)[, i]) / scale))
-        temp <-
-          data.frame(expand.grid(e = temp$x,
-                                 c = temp$y),
-                     z = as.vector(temp$z))
-        densitydf <-
-          rbind(densitydf, cbind(temp, rep(i, dim(temp)[[1]])))
-      }
-      
-      names(densitydf) <- c("delta_e", "delta_c", "z", "comparison")
-      densitydf$comparison <- as.factor(densitydf$comparison)
-      ceplane <-
-        ceplane +
-        geom_contour(aes(z = .data$z, colour = .data$comparison),
-                     data = densitydf,
-                     bins = nlevels) +
-        guides(colour = guide_legend(override.aes =
-                                       list(linetype = 0)))
-    } else {
-      ceplane <-
-        ceplane +
-        stat_density2d() +
-        guides(colour = guide_legend(override.aes =
-                                       list(linetype = 0)))
-    }
-  }
-
   theme_add <- purrr::keep(list(...), is.theme)
   
-  ceplane +
+  kd <- data.frame(
+    delta_e = c(as.matrix(he$delta_e)),
+    delta_c = c(as.matrix(he$delta_c)),
+    comparison =
+      as.factor(sort(
+        rep(1:he$n_comparisons, dim(as.matrix(he$delta_e))[1]))))
+  
+  labels.df <-
+    data.frame(
+      he = c(ranges$e[2], ranges$e[1], ranges$e[1], ranges$e[2]),
+      y = c(rep(ranges$c[2], 2), rep(ranges$c[1], 2)),
+      label = c(p.ne, p.nw, p.sw, p.se),
+      hjust = as.factor(c(1, 0, 0, 1)))
+  
+  contour_plot <-
+    ggplot(kd,
+           aes(x = .data$delta_e, y = .data$delta_c,
+               col = .data$comparison)) +
+    stat_density2d() +
+    geom_point(size = 1) +
+    scale_color_manual(label = comparisons.label,
+                       values = colors.label,
+                       na.value = "black") +
+    geom_text(data = labels.df,
+              aes(x = .data$he,
+                  y = .data$y,
+                  hjust = .data$hjust,
+                  label = .data$label),
+              parse = TRUE,
+              size = rel(3.5)) +
+    guides(colour = guide_legend(override.aes =
+                                   list(linetype = 0)))
+  
+  contour_plot +
     geom_hline(yintercept = 0, colour = "grey") +
     geom_vline(xintercept = 0, colour = "grey") +
     coord_cartesian(xlim = graph_params$xlim,
