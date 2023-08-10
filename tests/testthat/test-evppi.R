1c1457d2c5b788b2eb4ea0a09feb0f470ff51da8
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-08-10 14:19:31 +0100

    added evppi tests using code from BCEA book

--- tests/testthat/test-evppi.R
+++ tests/testthat/test-evppi.R
@@ -1,75 +1,113 @@
 
-library(BCEA)
+# library(BCEA)
 
-test_that("basic", {
+
+test_that("vaccine data", {
+  
+  data(Vaccine, package = "BCEA")
+  treats <- c("Status quo", "Vaccination")
+  
+  # Run the health economic evaluation using BCEA
+  bcea_vacc <- bcea(e.pts, c.pts, ref = 2, interventions = treats)
+  
+  # inputs
+  inp <- createInputs(vaccine_mat)
+  
+  expect_length(inp , 2)
+  expect_named(inp, c("mat", "parameters"))
+  expect_type(inp, "list")
+  
+  # Compute the EVPPI for a bunch of parameters
+  
+  # GAM regression
+  EVPPI <- evppi(bcea_vacc, c("beta.1." , "beta.2."), inp$mat)
+  
+  expect_s3_class(EVPPI, "evppi")
+  expect_length(EVPPI, 10)
+  expect_type(EVPPI, "list")
+  
+  # plot(EVPPI)
+  
+  # deprecated (single parameter) methods
+  # Strong & Oakley
+  EVPPI.so <- evppi(bcea_vacc, c("beta.1.", "beta.2."), inp$mat, method = "so", n.blocks = 50)
+  
+  expect_s3_class(EVPPI.so, "evppi")
+  expect_length(EVPPI.so, 6)
+  expect_type(EVPPI.so, "list")
+  
+  # Sadatsafavi et al
+  EVPPI.sad <- evppi(bcea_vacc, c("beta.1.", "beta.2."), inp$mat, method = "sad", n.seps = 1)
+  
+  expect_s3_class(EVPPI.sad, "evppi")
+  expect_length(EVPPI.sad, 6)
+  expect_type(EVPPI.sad, "list")
+  
+  ##TODO: errors
+  # plot(EVPPI.so)
+  # plot(EVPPI.sad)
+  
+  # Compute the EVPPI using INLA/SPDE
+  if (require("INLA")) {
+    x_inla <- evppi(he = bcea_vacc, 39:40, input = inp$mat)
+
+    expect_s3_class(x_inla, "evppi")
+    expect_length(x_inla, 10)
+    expect_type(x_inla, "list")
+  }
+  
+  # using GAM regression
+  x_gam <- evppi(he = bcea_vacc, 39:40, input = inp$mat, method = "GAM")
+  
+  expect_s3_class(x_gam, "evppi")
+  expect_length(x_gam, 10)
+  expect_type(x_gam, "list")
+  
+  # using Strong et al GP regression
+  x_gp <- evppi(he = bcea_vacc, 39:40, input = inp$mat, method = "GP")
+  
+  expect_s3_class(x_gp, "evppi")
+  expect_length(x_gp, 10)
+  expect_type(x_gp, "list")
+  
+  # # plot results
+  # if (require("INLA"))
+  #   plot(x_inla)
+  # points(x_inla$k, x_inla$evppi, type = "l", lwd = 2, lty = 2)
+  # points(x_gam$k, x_gam$evppi, type = "l", col = "red")
+  # points(x_gp$k, x_gp$evppi, type = "l", col = "blue")
+  # 
+  # if (require("INLA")) {
+  #   plot(x_inla$k, x_inla$evppi, type = "l", lwd = 2, lty = 2)
+  #   points(x_gam$k, x_gam$evppi, type = "l", col = "red")
+  #   points(x_gp$k, x_gp$evppi, type = "l", col = "blue")
+  # }
 })
 
 
-###############
-# vaccine data
-
-data(Vaccine, package = "BCEA")
-treats <- c("Status quo", "Vaccination")
-
-# Run the health economic evaluation using BCEA
-m <- bcea(e.pts, c.pts, ref = 2, interventions = treats)
-
-# Compute the EVPPI for a bunch of parameters
-inp <- createInputs(vaccine_mat)
-
-# GAM regression
-EVPPI <- evppi(m, c("beta.1." , "beta.2."), inp$mat)
-
-plot(EVPPI)
-
-# deprecated (single parameter) methods
-# Strong & Oakley
-EVPPI.so <- evppi(m, c("beta.1.", "beta.2."), inp$mat, method = "so", n.blocks = 50)
-# Sadatsafavi et al
-EVPPI.sad <- evppi(m, c("beta.1.", "beta.2."), inp$mat, method = "sad", n.seps = 1)
-
-##TODO: errors
-# plot(EVPPI.so)
-# plot(EVPPI.sad)
-
-# Compute the EVPPI using INLA/SPDE
-if (require("INLA"))
-  x_inla <- evppi(he = m, 39:40, input = inp$mat)
-
-# using GAM regression
-x_gam <- evppi(he = m, 39:40, input = inp$mat, method = "GAM")
-
-# using Strong et al GP regression
-x_gp <- evppi(he = m, 39:40, input = inp$mat, method = "GP")
-
-# plot results
-if (require("INLA"))
-  plot(x_inla)
-points(x_inla$k, x_inla$evppi, type = "l", lwd = 2, lty = 2)
-points(x_gam$k, x_gam$evppi, type = "l", col = "red")
-points(x_gp$k, x_gp$evppi, type = "l", col = "blue")
-
-if (require("INLA")) {
-  plot(x_inla$k, x_inla$evppi, type = "l", lwd = 2, lty = 2)
-  points(x_gam$k, x_gam$evppi, type = "l", col = "red")
-  points(x_gp$k, x_gp$evppi, type = "l", col = "blue")
-}
-
-###############
-# smoking data
-
-data(Smoking, package = "BCEA")
-treats <-
-  c("No intervention", "Self-help",
-    "Individual counselling", "Group counselling")
-
-m <- bcea(eff, cost, ref = 4, interventions = treats, Kmax = 500)
-
-##TODO: there is no smoking_output. what should we use?...
-inp <- createInputs(smoking_output)
-
-EVPPI <- evppi(m, param_idx = c(2,3), inp$mat, h.value = 5e-7)
-
-plot(EVPPI)
+test_that("smoking data", {
+  
+  data(Smoking, package = "BCEA")
+  treats <-
+    c("No intervention", "Self-help",
+      "Individual counselling", "Group counselling")
+  
+  bcea_smoke <- bcea(eff, cost, ref = 4, interventions = treats, Kmax = 500)
+  
+  # inputs
+  inp <- createInputs(smoking_output)
+  
+  expect_length(inp , 2)
+  expect_named(inp, c("mat", "parameters"))
+  expect_type(inp, "list")
+  
+  EVPPI <- evppi(bcea_smoke, param_idx = c(2,3), inp$mat, h.value = 5e-7)
+  
+  expect_s3_class(EVPPI, "evppi")
+  expect_length(EVPPI, 10)
+  expect_type(EVPPI, "list")
+  
+  # plot(EVPPI)
+})
 
 
