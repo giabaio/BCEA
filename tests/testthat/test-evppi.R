38ff5ff828ee29db9920439b999163e7c7e1217f
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-08-10 14:02:45 +0100

    added smoking-jags back in. cant remember why I took it out but I still might have to again!

--- tests/testthat/test-evppi.R
+++ tests/testthat/test-evppi.R
@@ -5,70 +5,71 @@ test_that("basic", {
 })
 
 
-# # 1. Runs the full Bayesian model and then produces the overall economic evaluation
-# source("Utils.R")	# loads some useful functions
-# source("runModel.R")	# runs the full model
-# data("Vaccine")
-# 
-# # using the 2-stage MCMC approach
-# source("EVPPI_2stage.R")
-# 
-# par.int <- beta[, 1]		# defines the parameter of interest
-# 
-# phi.name <-
-#   "beta.sim"		# the name of the parameter of interest in the data list
-# 
-# n.out <-
-#   1000			# number of outer simulations (to compute the mean wrt phi)
-# 
-# ec.vars <-
-#   "ComputeVars.R"	# file where the computations for e,c are stored
-# 
-# bcea.call <- "bcea(e=e.pts, c=c.pts, ref=2, interventions=treats)"
-# 
-# ev <-
-#   evppi2stage(phi = par.int,
-#                phi.name,
-#                n.out = n.out,
-#                m,
-#                data = dataJags,
-#                model.file = filein,
-#                params = params,
-#                inits = NULL,
-#                n.iter = 20000,
-#                n.burnin = 9500,
-#                n.thin = NULL,
-#                n.mc = 1000,
-#                working.dir = NULL,
-#                ec.vars,
-#                bcea.call)
-# 
-# # 3. Now computes the EVPPI using numerical approximation
-# 
-# source("evppi.R")
-# 
-# f <- numeric()		# will use to store the values of EVPI for each k
-# nb <- m$U		# extracts the net benefits from the full economic model
-# evppi.graphics <- FALSE
-# 
-# for (k in 1:length(m$k)) {
-#   nbs <-
-#     nb[, k, ]	# this is a n.sims x n.interventions matrix with the NB for each value of k
-#   
-#   f[k] <-
-#     evppi(par.int, nbs, c(2, 1, 1))$evppi	# EVPPI for each value of k
-# }
-# 
-# 
-# plot(m$k,
-#      ev$evppi,
-#      t = "l",
-#      lty = 2,
-#      col = "blue",
-#      # 1. the 2-stages MCMC EVPPI
-#      ylim = range(ev$evppi, f, m$evi))
-# 
-# points(m$k, f, t = "l", col = "red")			# 2. the numerical-approximated EVPPI
-# points(m$k, m$evi, t = "l")				# 3. the full EVPI (for reference)
+###############
+# vaccine data
+
+data(Vaccine, package = "BCEA")
+treats <- c("Status quo", "Vaccination")
+
+# Run the health economic evaluation using BCEA
+m <- bcea(e.pts, c.pts, ref = 2, interventions = treats)
+
+# Compute the EVPPI for a bunch of parameters
+inp <- createInputs(vaccine_mat)
+
+# GAM regression
+EVPPI <- evppi(m, c("beta.1." , "beta.2."), inp$mat)
+
+plot(EVPPI)
+
+# deprecated (single parameter) methods
+# Strong & Oakley
+EVPPI.so <- evppi(m, c("beta.1.", "beta.2."), inp$mat, method = "so", n.blocks = 50)
+# Sadatsafavi et al
+EVPPI.sad <- evppi(m, c("beta.1.", "beta.2."), inp$mat, method = "sad", n.seps = 1)
+
+##TODO: errors
+# plot(EVPPI.so)
+# plot(EVPPI.sad)
+
+# Compute the EVPPI using INLA/SPDE
+if (require("INLA"))
+  x_inla <- evppi(he = m, 39:40, input = inp$mat)
+
+# using GAM regression
+x_gam <- evppi(he = m, 39:40, input = inp$mat, method = "GAM")
+
+# using Strong et al GP regression
+x_gp <- evppi(he = m, 39:40, input = inp$mat, method = "GP")
+
+# plot results
+if (require("INLA"))
+  plot(x_inla)
+points(x_inla$k, x_inla$evppi, type = "l", lwd = 2, lty = 2)
+points(x_gam$k, x_gam$evppi, type = "l", col = "red")
+points(x_gp$k, x_gp$evppi, type = "l", col = "blue")
+
+if (require("INLA")) {
+  plot(x_inla$k, x_inla$evppi, type = "l", lwd = 2, lty = 2)
+  points(x_gam$k, x_gam$evppi, type = "l", col = "red")
+  points(x_gp$k, x_gp$evppi, type = "l", col = "blue")
+}
+
+###############
+# smoking data
+
+data(Smoking, package = "BCEA")
+treats <-
+  c("No intervention", "Self-help",
+    "Individual counselling", "Group counselling")
+
+m <- bcea(eff, cost, ref = 4, interventions = treats, Kmax = 500)
+
+##TODO: there is no smoking_output. what should we use?...
+inp <- createInputs(smoking_output)
+
+EVPPI <- evppi(m, param_idx = c(2,3), inp$mat, h.value = 5e-7)
+
+plot(EVPPI)
 
 
