a565620d1f2545ebccc1985dd0d31a52f2ad69ed
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-06-16 13:15:17 +0100

    merge andreas plotly stuff

--- R/make_legend_ggplot.R
+++ R/make_legend_ggplot.R
@@ -39,32 +39,17 @@ make_legend_ggplot <- function(dat, pos_legend) {
     }
   } else if (is.character(pos_legend) && pos_legend != "") {
     
-    # verify string is specified correctly
-    stopifnot(grepl("^(bottom|top)*(left|right)*$", pos_legend))
     pos_choices <- c("left", "right", "bottom", "top")
     dir_choices <- c("horizontal", "vertical")
     
-    # set default
-    legend_just <- "center"
+    legend_pos <- pos_choices[sapply(pos_choices, function(t) grepl(t, pos_legend))]
+    legend_dir <- dir_choices[sapply(dir_choices, function(t) grepl(t, pos_legend))]
     
-    # if legend_pos is a combined string such as "topright" or "bottomleft"
-    if (sapply(pos_choices, function(pattern) grepl(pattern, pos_legend)) |> sum() > 1) {
-      pos <- numeric(2)
-      pos[1] <- grepl(pos_choices[2], paste0(pos_legend, "$"))
-      pos[2] <- grepl(pos_choices[4], paste0("^", pos_legend))
-      legend_pos <- pos
-      # also overwrite justification for proper alignment in the plot corners
-      legend_just <- pos
-    } else {
-      legend_pos <- pos_choices[pmatch(pos_legend, pos_choices)]
-    }
-    
-    # restrict direction to a single value, NA is caught later
-    legend_dir <- dir_choices[sapply(dir_choices, function(t) grepl(t, pos_legend))][1]
-    
-    if (is.na(legend_dir)) {
+    if (length(legend_dir) == 0) {
       legend_dir <- NULL  # default
     }
+    
+    legend_just <- "center"
   } else if (is.numeric(pos_legend) &&
              length(pos_legend) == 2) {
     legend_pos <- pos_legend
